64fb3aa0cde301e4162dd156bf687d2e7023b366
gyugyu90 <kh2000park@gmail.com>
2020-02-12 10:48:45 +0900

    5장 예제

--- src/main/java/com/example/modernjava/part2/Part2.java
+++ src/main/java/com/example/modernjava/part2/Part2.java
@@ -26,6 +26,60 @@ public class Part2 {
                 .collect(Collectors.toList()); // terminal operation
         System.out.println(threeHighCaloricDishNames);
 
+        List<Dish> vegetarianDishes = menu.stream()
+                .filter(Dish::isVegetarian)
+                .collect(Collectors.toList());
+
+        List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+        numbers.stream()
+                .filter(i -> i % 2 == 0)
+                .distinct()
+                .forEach(System.out::println);
+
+        List<Dish> specialMenu = Arrays.asList(
+                new Dish("seasonal fruit", true, 120, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER)
+        );
+
+        List<Dish> filteredMenu = specialMenu.stream()
+                .filter(dish -> dish.getCalories() < 320)
+                .collect(Collectors.toList());
+
+        // slicing
+        List<Dish> sliceMenu1 = specialMenu.stream()
+                .takeWhile(dish -> dish.getCalories() < 320)
+                .collect(Collectors.toList()); // seasonal fruit, prawns
+
+        List<Dish> sliceMenu2 = specialMenu.stream()
+                .dropWhile(dish -> dish.getCalories() < 320)
+                .collect(Collectors.toList()); // rice, chicken, french fries
+
+        // 축소
+        List<Dish> dishes = specialMenu.stream()
+                .filter(dish -> dish.getCalories() > 300)
+                .limit(3)
+                .collect(Collectors.toList());
+
+        List<Dish> skippedDishes = menu.stream()
+                .filter(dish -> dish.getCalories() > 300)
+                .skip(2)
+                .collect(Collectors.toList());
+
+        // mapping
+        List<String> words = Arrays.asList("Modern", "Java", "In", "Action");
+        List<Integer> wordLengths = words.stream()
+                .map(String::length)
+                .collect(Collectors.toList());
+
+        words.stream()
+                .map(word -> word.split(""))
+                .flatMap(Arrays::stream)
+                .distinct()
+                .peek(System.out::println)
+                .collect(Collectors.toList());
     }
 
 }
