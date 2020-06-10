aee6b2c9f281a4fe87e49a64d7a693434943b0e3
gyugyu90 <kh2000park@gmail.com>
2020-06-10 09:57:33 +0900

    9.1.5 코드 유연성 개선

--- src/main/java/com/example/modernjava/part3/RefactorTestDebug.java
+++ src/main/java/com/example/modernjava/part3/RefactorTestDebug.java
@@ -1,5 +1,11 @@
 package com.example.modernjava.part3;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+
 public class RefactorTestDebug {
 
     public static void main(String[] args) {
@@ -20,7 +26,43 @@ public class RefactorTestDebug {
         };
         r2.run();
 
+        RefactorTestDebug refactorTestDebug = new RefactorTestDebug();
+
+        try {
+            String oneLine = refactorTestDebug.processFile((BufferedReader b) -> b.readLine());
+            System.out.println(oneLine);
+            String twoLines = refactorTestDebug.processFile((BufferedReader b) -> b.readLine() + b.readLine());
+            System.out.println(twoLines);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
     }
 
+    private String getFileFromResources(String fileName) {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file is not found!");
+        } else {
+            return resource.toString();
+        }
+
+    }
+
+    public String processFile(BufferedReaderProcessor p) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("data.txt").getFile());
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            return p.process(br); // 인수로 전달된 BufferedReaderProcessor 를 실행
+        }
+    }
+
+    interface BufferedReaderProcessor {
+        String process(BufferedReader b) throws IOException;
+    }
 
 }
